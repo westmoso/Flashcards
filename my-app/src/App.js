b04af88fdc29308a2c5203095710b8334affed19
westmoso <71048247+westmoso@users.noreply.github.com>
2020-10-28 19:13:22 -0400

    installed axios to test GET api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { front: '', back:[] };
+  }
+  async componentDidMount() {
+    const response = await axios.get(`http://localhost:5000/api/collections`);
+    this.setState({ back: response.data });
+    console.log(this.state.back)
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ my-app/src/components/cardslist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function cardslist() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
