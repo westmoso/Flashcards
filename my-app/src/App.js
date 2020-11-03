6a14ea3a2be68a605cf364b4495f855955992ee2
westmoso <71048247+westmoso@users.noreply.github.com>
2020-11-02 23:55:12 -0500

    created header class and drop down menu.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -31,9 +31,11 @@ class App extends Component {
   }
   render() {
     console.log("state", this.state);
+    
     if(this.state.posts.length > 0){
       return (
           <div>
+            <Header />
             <h2> click on the card to see the definition</h2>
             <Card data = {this.accessCards()} front={this.state.onFront}/>
           </div>
@@ -51,6 +53,43 @@ class App extends Component {
 
 }
 
+class Header extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        collection:'',
+      }
+  }
+
+  handleClick(){
+      this.setState({
+        collection: !this.state.front
+      });
+    }
+
+
+  render() {
+      return (
+          <div className='header'>
+              <div className='header-content header-content__left'>
+                  <a href="/">Home</a>
+              </div>
+              <div className='header-content header-content__middle'>
+               Add A Card
+        </div>
+              <div className='header-content header-content__right'>
+                  <div class="dropdown">
+                      <span>Pick A Deck</span>
+                      <div class="dropdown-content">
+                          <p>C#</p>
+                          <p>React</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      )
+  }
+}
 
 
 
--- my-app/src/cardComp/card.js
+++ my-app/src/cardComp/card.js
@@ -1,6 +1,5 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-//import './App.css';
+import '../App.css';
 
 
 class Card extends React.Component{
