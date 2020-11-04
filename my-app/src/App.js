af12acc3ce015f2fb0002960166a6e41e8e95172
Aj Steinbacher <adamjsteinbacher@gmail.com>
2020-11-03 20:52:39 -0600

    created buttons next card,last card, next collection,last collection, all working all tested

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,45 +11,73 @@ class App extends Component {
     currentCollection: 0,
     onFront: true
   }
+
+   
+  
+
   async componentDidMount(){
   let posts = Axios.get("http://localhost:5000/api/collections");
   posts = await posts;
   this.setState({ posts: posts.data });
   console.log(this.state.posts);
   }
-  accessCards(){
-  if(this.state.posts.length > 0){
-    console.log("card", this.state.posts[this.state.currentCollection].cards[this.state.currentCard])
-    return this.state.posts[this.state.currentCollection].cards[this.state.currentCard]
-  }
-  }
-  accessNextCard(){    /// ask about this one during 1-1
-  for(i = 0; i < this.state.posts.cards.length ; i++){
-    console.log("card", this.state.posts[this.state.currentCollection].cards[this.state.currentCard])
-    return this.state.posts[this.state.currentCollection].cards[this.state.currentCard]
-  }
-}
-  render(){
-  console.log("state", this.state);
-  if(this.state.posts.length > 0){
-    return (
-        <div>
-          <Header />
-          <h2>click on the card to see the definition</h2>
-          <Card data = {this.accessCards()} front={this.state.onFront}/>
-          <button id="nextCard" data = {this.accessNextCard()}>next card</button> 
-        </div>
-        )
-  }
-  else{
-    return (
-      <div>
 
-      </div>
-    )
+  accessCards(){
+    if(this.state.posts.length > 0){
+      console.log("card", this.state.posts[this.state.currentCollection].cards[this.state.currentCard])
+      return this.state.posts[this.state.currentCollection].cards[this.state.currentCard]
+    }
   }
+
+  goToNextCard(){
+    let currentCardNumber = this.state.currentCard + 1;
+    this.setState({currentCard: currentCardNumber});
   }
+  goBackACard(){
+    let currentCardNumber = this.state.currentCard - 1;
+    this.setState({currentCard: currentCardNumber});
+
+  }
+  goToNextCollection(){
+    let currentCollectionNumber = this.state.currentCollection + 1;
+    this.setState({currentCollection: currentCollectionNumber});
+  }
+  goBackACollection(){
+    let currentCollectionNumber = this.state.currentCollection - 1;
+    this.setState({currentCollection: currentCollectionNumber});
+
+  }
+
+  render(){
+    console.log("state", this.state);
+    if(this.state.posts.length > 0){
+      return (
+          <div>
+            <Header />
+            <h2>click on the card to see the definition</h2>
+            <Card data = {this.accessCards()} front={this.state.onFront}/>
+            <button onClick={() => this.goToNextCard()}>Next Card</button>
+            <button onClick={() => this.goBackACard()}>last Card</button>
+            <button onClick={() => this.goToNextCollection()}>Next Collection</button>
+            <button onClick={() => this.goBackACollection()}>last Collection</button>
+            
+            
+           
+          </div>
+          )
+    }
+    else{
+      return (
+        <div>
+  
+        </div>
+      )
+    }
+  }
+  
 }
+  
+
 
 
       
